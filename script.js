e0e99a339bdeb7e48a477e68b4729ff395180862
Glitch (mmosqueraherrera) <none>
2023-09-18 13:54:12 +0000

    👝👕 Checkpoint ./script.js copy:48343004/6271 ./script.js:48343004/38075 ./script.js echt:48343004/6271 ./java:48343004/6271 ./script.js testing:48343004/18326

--- script.js
+++ script.js
@@ -1,100 +1,46 @@
 // JavaScript-code for validation
 function validateForm() {
-  
     // Valideer persoonsgegevens
     var naam = document.getElementById("Naam").value;
     var adres = document.getElementById("Adres").value;
     var postcode = document.getElementById("Postcode").value;
     var plaats = document.getElementById("Plaats").value;
     var ontbrekendeGegevens = [];
-  
-  
-  //kleur van de balk wit maken nadat je iets invult//
 
-  document.getElementById("Naam").addEventListener("input", function () {
-  if (this.value.trim() !== "") {
-    this.style.backgroundColor = "white"; // Als er tekst is ingevoerd, maak de achtergrond wit
-  } else {
-    this.style.backgroundColor = "yellow"; // Als het veld leeg is, maak de achtergrond geel
-  }
-});
+    // Voeg event listeners toe om de balk wit te maken wanneer er tekst wordt ingevoerd
+    document.getElementById("Naam").addEventListener("input", function () {
+        if (this.value.trim() !== "") {
+            this.style.backgroundColor = "white";
+        } else {
+            this.style.backgroundColor = "yellow";
+        }
+    });
 
-document.getElementById("Adres").addEventListener("input", function () {
-  if (this.value.trim() !== "") {
-    this.style.backgroundColor = "white";
-  } else {
-    this.style.backgroundColor = "yellow";
-  }
-});
+    document.getElementById("Adres").addEventListener("input", function () {
+        if (this.value.trim() !== "") {
+            this.style.backgroundColor = "white";
+        } else {
+            this.style.backgroundColor = "yellow";
+        }
+    });
 
-document.getElementById("Postcode").addEventListener("input", function () {
-  if (this.value.trim() !== "") {
-    this.style.backgroundColor = "white";
-  } else {
-    this.style.backgroundColor = "yellow";
-  }
-});
+    document.getElementById("Postcode").addEventListener("input", function () {
+        if (this.value.trim() !== "") {
+            this.style.backgroundColor = "white";
+        } else {
+            this.style.backgroundColor = "yellow";
+        }
+    });
 
-document.getElementById("Plaats").addEventListener("input", function () {
-  if (this.value.trim() !== "") {
-    this.style.backgroundColor = "white";
-  } else {
-    this.style.backgroundColor = "yellow";
-  }
-});
-  
-  
-  //main code//
-  
-  if (naam === "") {
-    ontbrekendeGegevens.push("Naam");
-    document.getElementById("Naam").style.backgroundColor = "yellow";
-} else {
-    document.getElementById("Naam").style.backgroundColor = "white";
-}
+    document.getElementById("Plaats").addEventListener("input", function () {
+        if (this.value.trim() !== "") {
+            this.style.backgroundColor = "white";
+        } else {
+            this.style.backgroundColor = "yellow";
+        }
+    });
 
-    if (adres === "") {
-        ontbrekendeGegevens.push("Adres");
-        document.getElementById("Adres").style.backgroundColor = "yellow";
-    } else {
-        document.getElementById("Adres").style.backgroundColor = "white";
-    }
-
-    if (postcode === "") {
-        ontbrekendeGegevens.push("Postcode");
-        document.getElementById("Postcode").style.backgroundColor = "yellow";
-    } else {
-        document.getElementById("Postcode").style.backgroundColor = "white";
-    }
-
-    if (plaats === "") {
-        ontbrekendeGegevens.push("Plaats");
-        document.getElementById("Plaats").style.backgroundColor = "yellow";
-    } else {
-        document.getElementById("Plaats").style.backgroundColor = "white";
-    }
-
-    if (ontbrekendeGegevens.length > 0) {
-        var melding = "Vul de volgende persoonsgegevens in: " + ontbrekendeGegevens.join(", ");
-        alert(melding);
-        return false;
-    }
-  
-
-    // Valideer quizantwoorden
-    var vraag1 = document.querySelector('input[name="vraag1"]:checked');
-    var vraag2 = document.querySelector('input[name="vraag2"]:checked');
-    var vraag3 = document.querySelector('input[name="vraag3"]:checked');
-    var vraag4 = document.querySelectorAll('input[name="vraag4[]"]:checked');
-    var vraag5 = document.querySelector('input[name="vraag5"]:checked');
-
-    var antwoordenJuist = controleerAntwoorden();
-
-    if (!antwoordenJuist) {
-        alert("Niet alle quizvragen zijn correct beantwoord.");
-        return false;
-    }
-
+    // Valideer persoonsgegevens
     if (naam === "") {
         ontbrekendeGegevens.push("Naam");
         document.getElementById("Naam").style.backgroundColor = "yellow";
@@ -129,49 +75,71 @@ document.getElementById("Plaats").addEventListener("input", function () {
         return false;
     }
 
-    return true;
-}
+    // Valideer quizantwoorden
+    var vraag1 = document.querySelector('input[name="vraag1"]:checked');
+    var vraag2 = document.querySelector('input[name="vraag2"]:checked');
+    var vraag3 = document.querySelector('input[name="vraag3"]:checked');
+    var vraag4 = document.querySelectorAll('input[name="vraag4[]"]:checked');
+    var vraag5 = document.querySelector('input[name="vraag5"]:checked');
 
-function controleerAntwoorden() {
+    // Controleer of alle vragen zijn beantwoord
+    if (!vraag1 || !vraag2 || !vraag3 || vraag4.length === 0 || !vraag5) {
+        alert("Beantwoord alle quizvragen.");
+        return false;
+    }
+
+    // Controleer alle vragen en tel correcte antwoorden
     var juisteAntwoorden = {
         vraag1: "Parijs",
         vraag2: "Mars",
         vraag3: "Geel",
-        vraag4: ["Vis", "Olifant"], // Een array van juiste antwoorden
+        vraag4: ["Vis", "Olifant"],
         vraag5: "7"
     };
 
     var antwoorden = {
-        vraag1: document.querySelector('input[name="vraag1"]:checked'),
-        vraag2: document.querySelector('input[name="vraag2"]:checked'),
-        vraag3: document.querySelector('input[name="vraag3"]:checked'),
-        vraag4: document.querySelectorAll('input[name="vraag4[]"]:checked'),
-        vraag5: document.querySelector('input[name="vraag5"]:checked')
+        vraag1: vraag1.value,
+        vraag2: vraag2.value,
+        vraag3: vraag3.value,
+        vraag4: Array.from(vraag4, input => input.value),
+        vraag5: vraag5.value
     };
 
-    var juiste = true;
+    var correcteAntwoorden = 0;
+    var totaalVragen = 0;
 
     for (var vraag in juisteAntwoorden) {
+        totaalVragen++;
         if (Array.isArray(juisteAntwoorden[vraag])) {
             // Controleer checkbox-vragen
-            var geselecteerdeOpties = [];
-            antwoorden[vraag].forEach(function(optie) {
-                geselecteerdeOpties.push(optie.value);
-            });
-            if (!arraysVergelijken(juisteAntwoorden[vraag], geselecteerdeOpties)) {
-                juiste = false;
-                break;
+            if (arraysVergelijken(juisteAntwoorden[vraag], antwoorden[vraag])) {
+                correcteAntwoorden++;
             }
         } else {
             // Controleer radiobutton-vragen
-            if (antwoorden[vraag].value !== juisteAntwoorden[vraag]) {
-                juiste = false;
-                break;
+            if (juisteAntwoorden[vraag] === antwoorden[vraag]) {
+                correcteAntwoorden++;
             }
         }
     }
 
-    return juiste;
+    // Toon een popup met juiste antwoorden en het percentage correcte antwoorden
+    var juisteAntwoordenTekst = "Juiste antwoorden waren:\n" +
+        "Vraag 1: Parijs\n" +
+        "Vraag 2: Mars\n" +
+        "Vraag 3: Geel\n" +
+        "Vraag 4: Vis, Olifant\n" +
+        "Vraag 5: 7";
+
+    var percentageCorrect = (correcteAntwoorden / totaalVragen) * 100;
+
+    var popupTekst = "Succesvol ingeleverd!\n\n" +
+        juisteAntwoordenTekst + "\n\n" +
+        "Je had " + percentageCorrect.toFixed(2) + "% van de vragen correct.";
+
+    alert(popupTekst);
+
+    return true;
 }
 
 function arraysVergelijken(arr1, arr2) {
@@ -180,23 +148,4 @@ function arraysVergelijken(arr1, arr2) {
         if (arr1[i] !== arr2[i]) return false;
     }
     return true;
-}
-
-
-function toonJuisteAntwoorden() {
-    var juisteAntwoorden = {
-        vraag1: "Parijs",
-        vraag2: "Mars",
-        vraag3: "Geel",
-        vraag4: ["Vis"],
-        vraag5: "7"
-    };
-
-    var juisteAntwoordenTekst = "Juiste antwoorden:\n";
-    for (var vraag in juisteAntwoorden) {
-        juisteAntwoordenTekst += "Vraag " + vraag.substr(5) + ": " + juisteAntwoorden[vraag] + "\n";
-    }
-
-    alert(juisteAntwoordenTekst);
-}
-
+}; 
